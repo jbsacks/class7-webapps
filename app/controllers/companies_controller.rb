df400d0c68a93a743a6a0187646eac19dc608a68
jbsacks <josh.sacks@kellogg.northwestern.edu>
2022-05-12 01:04:40 +0000

    commit pt1

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,8 +4,14 @@ class CompaniesController < ApplicationController
   # def index
   # end
 
+  def index 
+    @companies = Company.all 
+  end 
+
   def show
     @company = Company.find_by({ "id" => params["id"] })
   end
+
+
 
 end
